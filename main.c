cf5e3b82bb3a6ed9d078ce3b320c7a2bd7cef6de
leuteris99 <leualexiou@hotmail.com>
2019-12-10 19:48:21 +0200

    FINISH IT!

--- main.c
+++ main.c
@@ -34,21 +34,27 @@ int main(int argc, char **argv) {
             printf("Readied successful: %s\n", buf);
         }
 
-        close(p[0]);
-        write(p[1], buf, strlen(buf) + 1);
+        dup2(p[1], STDOUT_FILENO); // post
+        close(p[0]); //pre
+        close(p[1]); //post
+        //write(p[1], buf, strlen(buf) + 1); //pre
         close(fd);
-        printf("complete parent!\n");
+        //printf("complete parent!\n");
+        printf("%s",buf);
     } else if (pid == 0) {
         //child
         printf("hi from child\n");
         printf("enter critical section child!\n");
 
-        close(p[1]);
-        read(p[0], buf, bufSize);
+        dup2(p[0],STDIN_FILENO); // post
+        close(p[1]); // pre
+        close(p[0]); //post
+        //read(p[0], buf, bufSize); //pre
         printf("the data is: %s\n", buf);
         printf("complete child!\n");
-        char *args[]={"tr","\"[:lower:]\"","\"[:upper:]\""}; // TODO: fix the exec.
-        execl("/bin/ls","ls","-l",NULL);
+        //char *args[]={"tr","\"[:lower:]\"","\"[:upper:]\""}; // TODO: fix the exec.
+        execl("/usr/bin/tr","tr","[:lower:]","[:upper:]",NULL);
+        //execl("/bin/ls","ls","-l",NULL);
         printf("the exec didn't work :(");
     } else{
         perror("fork error: ");
